f5dffa467804d914a390a321e93ac73e76838024
Triesha Sonarkar <147965443+trieshasonarkar1224@users.noreply.github.com>
2024-09-20 17:11:42 +0530

    added

---
+++ intro to react/src/Header.jsx
@@ -0,0 +1,20 @@
+function Header(){
+    return(
+        <header>
+            <h1>My Website</h1>
+            <nav>
+                <ul>
+                    <li><a herf="#">Home</a></li>
+                    <li><a herf="#">About</a></li>
+                    <li><a herf="#">Service</a></li>
+                    <li><a herf="#">Contact</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    )
+}
+
+
+
+export default Header 
