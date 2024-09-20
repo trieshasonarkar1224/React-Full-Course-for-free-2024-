5fd8107148973616291a8c68bf68f92ff3bf68a1
Triesha Sonarkar <147965443+trieshasonarkar1224@users.noreply.github.com>
2024-09-20 21:30:45 +0530

    added

---
+++ add CSS styles 🎨/src/App.jsx
@@ -0,0 +1,17 @@
+
+// HOW TO STYLE REACT COMPONENTS WITH CSS 
+// 
+// (not including external frameworks or preprocessors) 
+// 1. EXTERNAL   
+// 2. MODULES 
+// 3. INLINE 
+
+import Button from './Button.jsx' 
+
+function App() { 
+  
+  return(<Button />); 
+
+} 
+
+export default App
